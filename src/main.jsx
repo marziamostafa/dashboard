fae8d84c87035840552e613f9b782cea78f94c47
marzia <marziamostafa@gmail.com>
2025-11-26 08:11:21 +0600

    project setup -done

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./router/router.jsx";
+import { RouterProvider } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+      {/* <App /> */}
+    </React.StrictMode>
+  </StrictMode>
+);
